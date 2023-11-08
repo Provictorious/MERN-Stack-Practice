cefb8b2ac80fb89f61948b666fb703d5d09e38e2
Provictorious <provi77684@gmail.com>
2023-11-09 01:35:45 +0600

    done

---
+++ Module-13_Assignment_Final_Version/src/App.jsx
@@ -0,0 +1,22 @@
+import { Route, Routes } from "react-router-dom";
+import "./App.css";
+import { Navbar } from "./components/Navbar";
+import { About, Contact, Home, Services, Blog, Projects } from "./components/pages";
+
+function App() {
+  return (
+    <div className="App">
+      <Navbar />
+      <Routes>
+        <Route path="/" element={<Home />} />
+        <Route path="/about" element={<About />} />
+        <Route path="/services" element={<Services />} />
+        <Route path="/blog" element={<Blog />} />
+        <Route path="/projects" element={<Projects />} />
+        <Route path="/contact" element={<Contact />} />
+      </Routes>
+    </div>
+  );
+}
+
+export default App;
